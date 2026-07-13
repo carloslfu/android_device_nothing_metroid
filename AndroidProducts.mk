84fe0860d778e46aada637c5209f117279a84e13
Carlos Galarza <carloslfu@gmail.com>
2026-07-13 16:17:43 -0500

    Map the Lineage phone product target

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_metroid.mk \
-    $(LOCAL_DIR)/phone_md_metroid.mk
+    lineage_phone_md_metroid:$(LOCAL_DIR)/phone_md_metroid.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_metroid-bp2a-user \
