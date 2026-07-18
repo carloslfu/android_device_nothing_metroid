8e8f039c7e256fbb38967ad89a2cf9d4d3084c01
Carlos Galarza <carloslfu@gmail.com>
2026-07-18 09:28:35 -0500

    Install the WCN7750 firmware links

--- device.mk
+++ device.mk
@@ -116,7 +116,10 @@ PRODUCT_PACKAGES +=     wpa_supplicant     wpa_cli \
                         android.hardware.wifi.hostapd-V2-ndk \
                         android.hardware.wifi-service.metroid \
                         libwifi-hal-qcom.metroid \
-                        libwifi-hal-ctrl.metroid
+                        libwifi-hal-ctrl.metroid \
+                        firmware_wlanmdsp.otaupdate_symlink \
+                        firmware_wlan_mac.bin_symlink \
+                        firmware_WCNSS_qcom_cfg.ini_symlink
 # Display HALs from source (b20): ~23s reset fix. Module names mirror onyx un_dt device.mk
 # (same SoC sun/sm8750); composer-service added (essential, buildable in sm8750/display tree).
 # Several stock camera/display blobs still link both allocator AIDL V1 and V2.
